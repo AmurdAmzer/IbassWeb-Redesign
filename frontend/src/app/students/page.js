c601a11f35027b36ecce5a920780c6592e76de03
AmurdAmzer <amurdamzer@gmail.com>
2025-06-09 12:13:03 -0400

    enhancing student page and adding src section

--- frontend/src/app/students/page.js
+++ frontend/src/app/students/page.js
@@ -8,25 +8,25 @@ export default function StudentsPage() {
           name: 'Ashraf Muftawu',
           position: 'Boys Prefect',
           description: '2022/2023',
-          image: '/images/headmaster.jpg'
+          image: '/images/ashraf.jpg'
         },
         {
           name: 'Abubakar Siddick Mohammed',
           position: 'Assist Boys Prefect' ,
           description: '2022/2023',
-          image: '/images/assistant1.jpg'
+          image: '/images/sidik.jpg'
         },
         {
-          name: 'Mrs. Mariam Issah',
-          position: 'Assistant Headmistress',
-          description: 'Passionate about student welfare and academic support, Mrs. Issah oversees pastoral care and creates a nurturing environment for all students to thrive.',
-          image: '/images/assistant2.jpg'
+          name: 'Suwaiba Osman',
+          position: 'Main Girls Prefect',
+          description: '2022/2023',
+          image: '/images/suwaiba.jpg'
         },
         {
-          name: 'Ustaaz',
-          position: 'Imaam',
-          description: 'Our spiritual guide who provides Islamic education and moral guidance, helping students develop strong character and values alongside their academic pursuits.',
-          image: '/images/ustaaz.jpg'
+          name: 'Hawa Kabiru',
+          position: 'Assist Girls Prefect',
+          description: '2022/2023',
+          image: '/images/hawa.jpeg'
         }
     ];
 
@@ -41,13 +41,61 @@ export default function StudentsPage() {
               </p>
             </Container>
           </section>
+          
+          {/* SRC Section */}
+          <section className="py-5" style={{backgroundColor: '#e9ecef'}}>
+            <Container className="px-3 px-md-0">
+              <Row className="align-items-stretch">
+                <Col lg={6} md={12} className="mb-4 mb-lg-0 order-2 order-lg-1">
+                    <img 
+                        src="/images/src.png" 
+                        alt="About Ibadur Rahman Academy" 
+                        className="rounded src-image"
+                        style={{ 
+                        width: '100%', 
+                        height: 'auto',
+                        objectFit: 'cover',
+                        }}
+                    />
+                </Col>
+
+                <Col lg={6} md={12} className="order-1 order-lg-2">
+                  <Row>
+                    {/* SRC Card */}
+                    <Col md={12} className="mb-4">
+                      <Card className="h-100 text-center border-0">
+                        <Card.Body className="p-4">
+                          <h2 className="text-center mb-3" style={{ color: 'var(--school-blue)'}}>
+                          Students' Representative Council (SRC)
+                          </h2>
+                          <p className="mb-2 text-muted">
+                          About Ibadur Rahman Academy SRC
+                          </p>
+                          <p className="mb-2 text-muted">
+                          The SRC was formed in the year 2021 when the first sets of students were admitted into the school. The SRC is the highest student leadership group.
+                          </p>
+                          <p className="mb-2 text-muted">
+                          The SRC main aim is to champion the cause of students and laise between the student body, the school administration and those outside the school community.
+                          </p>
+                          <p className="mb-2 text-muted">
+                          It is remarkable to say that through the instrumentality of this council, this school stands among the few institutions in the country which have never gone on a violent demonstration, a clear testimony of the prevailing understanding between students and authorities.
+                          </p>
+                        </Card.Body>
+                      </Card>
+                    </Col>
+
+                  </Row>
+                </Col>
+              </Row>
+            </Container>
+          </section>
 
           {/* Prefects Section */}
-          <section className="py-5" style={{backgroundColor: '#e9ecef'}}>
+          <section className="py-5">
             <Container className="px-4 px-md-0">
               <h2 className="text-center mb-3" style={{background: 'linear-gradient(to right, transparent, var(--school-blue), transparent) no-repeat bottom', backgroundSize: '80% 3px', paddingBottom: '15px', width: 'fit-content', margin: '0 auto 1rem auto'}}>Student Leaders</h2>
               <p className="text-center text-muted mb-5 px-3">
-                Our experienced leadership team guiding the school towards academic excellence and holistic development.
+              Below is the list of School Prefects for 2022/2023 academic year.
               </p>
               <Row>
                 {prefects.map((member, index) => (
